b3f1793b8e8c614b973c660a05c6c86a8a660c67
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-08-11 10:25:52 +0500

    Добавляет компонент Filter

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Header from '../header/header.jsx';
 import Tabs from '../tabs/tabs.jsx';
+import Filter from '../filter/filter.jsx';
 import MapBlock from '../map-block/map-block.jsx';
 import CardsList from '../cards-list/cards-list.jsx';
 
@@ -21,25 +22,8 @@ const Main = ({rents, titles}) => {
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
               <b className="places__found">{rents} places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-
-              </form>
-
+              <Filter />
               <CardsList titles={titles} />
-
             </section>
 
             <MapBlock />
