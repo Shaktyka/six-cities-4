adbdb533de9a413eb265accc72068c98d6fee3f8
Shaktyka <k-i-s-u-n-j-a@list.ru>
2020-08-12 17:21:00 +0500

    Добавляет рендеринг списка инпутов для формы

--- src/components/reviews-form/reviews-form.jsx
+++ src/components/reviews-form/reviews-form.jsx
@@ -1,65 +1,62 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
+import {ReviewParams, RatingToText} from '../../consts.js';
 
 const ReviewsForm = () => {
   return (
-    <form className="reviews__form form" action="#" method="post">
+    <form
+      className="reviews__form form"
+      action="#"
+      method="post"
+      onSubmit={(evt) => {
+        evt.preventDefault();
+      }}
+    >
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-
-        <input className="form__rating-input visually-hidden" name="rating"
-          value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating"
-          value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating"
-          value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating"
-          value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-
-        <input className="form__rating-input visually-hidden" name="rating"
-          value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
+        {
+          [...new Array(5)].map((el, i) => {
+            return (
+              <React.Fragment key={i}>
+                <input
+                  className="form__rating-input visually-hidden"
+                  name="rating"
+                  value={i + 1}
+                  id={`${i + 1}-stars`}
+                  type="radio"
+                  onClick={() => {
+                    // evt.target.value
+                  }}
+                />
+                <label
+                  htmlFor={`${i + 1}-stars`}
+                  className="reviews__rating-label form__rating-label"
+                  title={RatingToText[i + 1]}>
+                  <svg className="form__star-image" width="37" height="33">
+                    <use xlinkHref="#icon-star"></use>
+                  </svg>
+                </label>
+              </React.Fragment>
+            );
+          }).reverse()
+        }
       </div>
 
       <textarea
         className="reviews__textarea form__textarea"
         id="review"
         name="review"
-        placeholder="Tell how was your stay, what you like and what can be improved">
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        minLength={ReviewParams.MIN_LENGTH}
+        maxLength={ReviewParams.MAX_LENGTH}
+      >
       </textarea>
 
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span>
           and describe your stay with at least
-          <b className="reviews__text-amount"> 50 characters</b>.
+          <b className="reviews__text-amount"> {ReviewParams.MIN_LENGTH} characters</b>.
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled="">
           Submit
--- src/consts.js
+++ src/consts.js
@@ -19,3 +19,16 @@ export const LogoFooterSize = {
   WIDTH: 64,
   HEIGHT: 33
 };
+
+export const ReviewParams = {
+  MIN_LENGTH: 50,
+  MAX_LENGTH: 300
+};
+
+export const RatingToText = {
+  1: `terribly`,
+  2: `badly`,
+  3: `not bad`,
+  4: `good`,
+  5: `perfect`
+};
